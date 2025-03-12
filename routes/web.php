ca2ee72859cdec143d0da83f0f4095a98a937c5d
Ega Bagus Purnama <egabagus1@gmail.com>
2025-03-12 22:32:52 +0700

    Initial Commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\Master\ProductController;
+use App\Livewire\Settings\Appearance;
+use App\Livewire\Settings\Password;
+use App\Livewire\Settings\Profile;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Route::get('settings/profile', Profile::class)->name('settings.profile');
+    Route::get('settings/password', Password::class)->name('settings.password');
+    Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
+
+    Route::prefix('dashboard')->group(function () {
+        Route::get('/products', [ProductController::class, 'index'])->name('products');
+    });
+});
+
+require __DIR__ . '/auth.php';
